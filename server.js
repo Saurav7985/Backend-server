48a7c0964212d53119f499155a228e86beb245f6
Saurav7985 <sauravraikwar26@gamil.com>
2026-02-20 13:55:11 +0530

    render link

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const app = require('./src/app')
 const connect = require('./src/config/database')
 require('dotenv').config()
-
-
+const port = process.env.PORT || 3000 
 
 connect()
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("server is started");
 }) 
